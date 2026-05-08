701db1702fc29c6fd5ea0a041a8a14662ab0c476
Youngho Cho <dominoyh5@gmail.com>
2026-05-08 16:25:28 +0900

    feat: pci driver skeleton

--- my_qemu_edu.c
+++ my_qemu_edu.c
@@ -1,17 +1,33 @@
 #include <linux/module.h>
+#include <linux/pci.h>
 
-static int __init my_qemu_edu_init(void)
+#define DRIVER_NAME "my_qemu_edu"
+#define VENDOR_ID 0x1234
+#define PRODUCT_ID 0x11e8
+
+static const struct pci_device_id my_qemu_edu_ids[] = {
+	{ PCI_DEVICE(VENDOR_ID, PRODUCT_ID) },
+	{ 0, }
+};
+
+static int my_qemu_edu_probe(struct pci_dev *pdev, const struct pci_device_id *id)
 {
-	pr_info("my_qemu_edu: loaded\n");
+	pr_info("my_qemu_edu: probe\n");
 	return 0;
 }
 
-static void __exit my_qemu_edu_exit(void)
+static void my_qemu_edu_remove(struct pci_dev *pdev)
 {
-	pr_info("my_qemu_edu: unloaded\n");
+	pr_info("my_qemu_edu: remove\n");
 }
 
-module_init(my_qemu_edu_init);
-module_exit(my_qemu_edu_exit);
+static struct pci_driver my_qemu_edu_driver = {
+	.name = DRIVER_NAME,
+	.id_table = my_qemu_edu_ids,
+	.probe = my_qemu_edu_probe,
+	.remove = my_qemu_edu_remove,
+};
+
+module_pci_driver(my_qemu_edu_driver);
 
 MODULE_LICENSE("GPL");
